2e9cae2d3242cff620f2edcc90759ea6215f52c7
Cheri M <cmerrym1@kent.edu>
2022-01-17 22:47:47 -0500

    readme working

---
+++ Develop/generatedREADME.md
@@ -0,0 +1,26 @@
+# README Generator
+This application takes user input and creates a README.md
+## Table of Contents:
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+### Installation:
+To install run the following from the command line:
+"npm install"
+### Usage:
+Nothing, it's pretty straight forward
+### License:
+This project is licensed under:
+MIT
+### Contributing:
+By contacting me on GitHub or via email
+### Tests:
+To test, type the following into the command line:
+"node index.js"
+### Questions:
+If you have any questions contact me on [GitHub](https://github.com/cmerrym1) or contact 
+Cheri Merryman at cmerrym1@kent.edu  
+ 
--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,97 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const writeFileAsync = util.promisify(fs.writeFile);
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const questions = () =>
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your name?"
+        },
+        {
+            type: "input",
+            name: "username",
+            message: "What is your GitHub username?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your email address?"
+        },
+        {
+            type: "input",
+            name: "title",
+            message: "Enter the title of our project:"
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Write a summary of your application:"
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "What kind of license should your project have?",
+            choices: ["MIT","APACHE 2.0","None"]
+        },
+        {
+            type: "input",
+            name: "installations",
+            message: "How does a user install this application?"
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "How does a user test this application?"
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "What does the user need to know about using this application?"
+        },
+        {
+            type: "input",
+            name: "contribute",
+            message: "How can a user contribute to this project?",
+        },
+    ]);
 
-// TODO: Create a function to initialize app
-function init() {}
 
-// Function call to initialize app
-init();
+function generateMD(data){    
+return`# ${data.title}
+${data.description}
+## Table of Contents:
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+### Installation:
+To install run the following from the command line:
+"npm install"
+### Usage:
+${data.usage}
+### License:
+This project is licensed under:
+${data.license}
+### Contributing:
+${data.contribute}
+### Tests:
+To test, type the following into the command line:
+"node index.js"
+### Questions:
+If you have any questions contact me on [GitHub](https://github.com/${data.username}) or contact 
+${data.name} at ${data.email}  
+ `
+}
+
+
+questions()
+.then((data) => writeFileAsync('generatedREADME.md', 
+generateMD(data)))
+    .then(() => console.log('Successfully wrote to index.html'))
+    .catch((err) => console.error(err));
